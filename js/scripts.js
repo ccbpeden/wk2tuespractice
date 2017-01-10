1488c45e4fdfc94d555723e554c17e545e1a813d
Charles Peden and Maggie Harrington <maggie@>
2017-01-10 10:34:28 -0800

    add font change function using addClass

--- js/scripts.js
+++ js/scripts.js
@@ -19,4 +19,33 @@ $(document).ready(function() {
   $("#tbd").click(function() {
     $("#superfancy").toggle();
   });
+  $("#comicsans").click(function(){
+    if ($("body").hasClass("font-arial")){
+     $("body").removeClass("font-arial");
+    };
+    if ($("body").hasClass("font-timesnewroman")){
+     $("body").removeClass("font-timesnewroman");
+    };
+    $("body").addClass("font-CS");
+  });
+
+  $("#arial").click(function(){
+    if ($("body").hasClass("font-CS")){
+      $("body").removeClass("font-CS");
+    };
+    if ($("body").hasClass("font-timesnewroman")){
+      $("body").removeClass("font-timesnewroman");
+    };
+    $("body").addClass("font-arial");
+  });
+
+  $("#timesnewroman").click(function(){
+    if ($("body").hasClass("font-arial")){
+      $("body").removeClass("font-arial");
+    };
+    if ($("body").hasClass("font-CS")){
+      $("body").removeClass("font-CS");
+    };
+    $("body").addClass("font-timesnewroman");
+  });
 });
